8fb47a456af63282ede57ab33bb7bfda6646a536
Matt Dupree <kmdupr33@gmail.com>
2017-05-04 23:03:57 -0400

    update for rxjava 2

--- lib/src/main/java/com/philosophicalhacker/lib/RxLoader.java
+++ lib/src/main/java/com/philosophicalhacker/lib/RxLoader.java
@@ -4,133 +4,107 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v7.app.AppCompatActivity;
-
-import rx.Emitter;
-import rx.Observable;
-import rx.Observer;
-import rx.functions.Action1;
-
-/**
- * Created by mattdupree on 7/23/16.
- */
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 public class RxLoader {
 
-    public static <T> Observable.Transformer<T, T> from(final AppCompatActivity activity) {
-        return from(activity, 0, false);
+  public static <T> ObservableTransformer<T, T> from(LoaderManager loaderManager,
+      final Context context) {
+    return from(loaderManager, context, 0, false);
+  }
+
+  public static <T> ObservableTransformer<T, T> from(final LoaderManager loaderManager,
+      final Context context, final int id, final boolean forceReload) {
+    return new ObservableTransformer<T, T>() {
+      @Override public ObservableSource<T> apply(@NonNull Observable<T> upstream) {
+        return Observable.create(
+            new LoaderCallbackAsyncEmitter<>(upstream, context,
+                loaderManager, id,
+                forceReload));
+      }
+    };
+  }
+
+  public static <T> ObservableTransformer<T, T> from(final LoaderManager loaderManager,
+      Context context, final int id) {
+    return from(loaderManager, context, id, false);
+  }
+
+  private static class ObservableLoader<T> extends Loader<T> {
+
+    private final Observable<T> mTObservable;
+    private Throwable mError;
+
+    ObservableLoader(Context context, Observable<T> tObservable) {
+      super(context);
+      mTObservable = tObservable;
     }
 
-    public static <T> Observable.Transformer<T, T> from(final AppCompatActivity activity,
-                                                        final int id, final boolean forceReload) {
-        return new Observable.Transformer<T, T>() {
-            @Override
-            public Observable<T> call(Observable<T> tObservable) {
-                return Observable.fromEmitter(
-		                new LoaderCallbackAsyncEmitter<>(tObservable, activity,
-                                activity.getSupportLoaderManager(),
-                                id, forceReload), Emitter.BackpressureMode.DROP);
+    @Override protected void onStartLoading() {
+      super.onStartLoading();
+      forceLoad();
+    }
+
+    @Override protected void onForceLoad() {
+      super.onForceLoad();
+      mTObservable.subscribe(new Consumer<T>() {
+        @Override public void accept(@NonNull T t) throws Exception {
+          deliverResult(t);
+        }
+      }, new Consumer<Throwable>() {
+        @Override public void accept(@NonNull Throwable throwable) throws Exception {
+          mError = throwable;
+          deliverResult(null);
+        }
+      });
+    }
+  }
+
+  private static class LoaderCallbackAsyncEmitter<T> implements ObservableOnSubscribe<T> {
+    private final Observable<T> mTObservable;
+    private final Context mContext;
+    private final LoaderManager mLoaderManager;
+    private final int mId;
+    private final boolean mForceReload;
+
+    LoaderCallbackAsyncEmitter(Observable<T> tObservable, Context context,
+        LoaderManager loaderManager, int id, boolean forceReload) {
+      mTObservable = tObservable;
+      mContext = context;
+      mLoaderManager = loaderManager;
+      mId = id;
+      mForceReload = forceReload;
+    }
+
+    @Override public void subscribe(@NonNull final ObservableEmitter<T> e) throws Exception {
+      final Loader<T> tLoader =
+          mLoaderManager.initLoader(mId, null, new LoaderManager.LoaderCallbacks<T>() {
+            @Override public Loader<T> onCreateLoader(int id, Bundle args) {
+              return new ObservableLoader<>(mContext, mTObservable);
             }
-        };
-    }
 
-    public static <T> Observable.Transformer<T, T> from(final AppCompatActivity activity,
-                                                        final int id) {
-        return from(activity, id, false);
-    }
-
-    private static class ObservableLoader<T> extends Loader<T> {
-
-        private final Observable<T> mTObservable;
-        private Throwable mError;
-
-        ObservableLoader(Context context, Observable<T> tObservable) {
-            super(context);
-            mTObservable = tObservable;
-        }
-
-        @Override
-        protected void onStartLoading() {
-            super.onStartLoading();
-            forceLoad();
-        }
-
-        @Override
-        protected void onForceLoad() {
-            super.onForceLoad();
-            mTObservable.subscribe(new Observer<T>() {
-                @Override
-                public void onCompleted() {
-                }
-
-                @Override
-                public void onError(Throwable e) {
-                    mError = e;
-                    deliverResult(null);
-                }
-
-                @Override
-                public void onNext(T t) {
-                    deliverResult(t);
-                }
-            });
-        }
-    }
-
-    private static class LoaderCallbackAsyncEmitter<T> implements Action1<Emitter<T>> {
-        private final Observable<T> mTObservable;
-        private final Context mContext;
-        private final LoaderManager mLoaderManager;
-        private final int mId;
-        private final boolean mForceReload;
-
-        LoaderCallbackAsyncEmitter(Observable<T> tObservable, Context context,
-                                   LoaderManager loaderManager,
-                                   int id,
-                                   boolean forceReload) {
-            mTObservable = tObservable;
-            mContext = context;
-            mLoaderManager = loaderManager;
-            mId = id;
-            mForceReload = forceReload;
-        }
-
-        @Override
-        public void call(final Emitter<T> emitter) {
-            final Loader<T> tLoader
-                    = mLoaderManager.initLoader(mId, null,
-                    new LoaderManager.LoaderCallbacks<T>() {
-                        @Override
-                        public Loader<T> onCreateLoader(
-                                int id,
-                                Bundle args) {
-                            return new ObservableLoader<>(
-                                    mContext,
-                                    mTObservable);
-                        }
-
-                        @Override
-                        public void onLoadFinished(
-                                Loader<T> loader,
-                                T data) {
-                            final Throwable error = ((ObservableLoader) loader).mError;
-                            if (error != null) {
-                                emitter.onError(
-                                        error);
-                            } else {
-                                emitter.onNext(data);
-                                emitter.onCompleted();
-                            }
-                        }
-
-                        @Override
-                        public void onLoaderReset(
-                                Loader<T> loader) {
-                        }
-                    });
-            if (mForceReload) {
-                tLoader.forceLoad();
+            @Override public void onLoadFinished(Loader<T> loader, T data) {
+              final Throwable error = ((ObservableLoader) loader).mError;
+              if (error != null) {
+                e.onError(error);
+              } else {
+                e.onNext(data);
+                e.onComplete();
+              }
             }
-        }
+
+            @Override public void onLoaderReset(Loader<T> loader) {
+            }
+          });
+      if (mForceReload) {
+        tLoader.forceLoad();
+      }
     }
+  }
 }
